177643ceb9605f432fbf209e549760f3f65f8a52
narko <gasong.sunarko@gmail.com>
2017-04-07 03:38:48 +0700

    error

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -82,7 +82,7 @@ $app->middleware([
 // $app->register(App\Providers\AppServiceProvider::class);
 // $app->register(App\Providers\AuthServiceProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
-$app->register('CatchAllOptionsRequestsProvider');
+$app->register('App\Providers\CatchAllOptionsRequestsProvider');
 
 /*
 |--------------------------------------------------------------------------
